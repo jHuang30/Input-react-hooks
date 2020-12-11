64ca3e1a3aca5bb8776e7db0ceb7a92f53c107dc
jhuang30 <jieyinghuang30@gmail.com>
2020-12-11 17:41:39 -0500

    a bit css

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
 import React, { useState }from 'react';
-import ShowInput from './pages/ShowInput'
+import ShowInput from './pages/ShowInput';
+import './App.css';
 
 function App() {
   const value1 = false
-  const value2 = ['apple', 'pear', 'banana']
-  const value3 = 'anything'
+  const value2 = ["Knock knock.", "Who’s there?", "Atch.", "Atch who?", "Bless you!"]
+  const value3 = 'The value of the prop'
   const[input, setInput] = useState(value1);
   return (
-    <>
-      Choose from below: 
+    <div className="wrapper">
+      Choose one below:
       <br />
       <br />
-      <button onClick={(e) => setInput(value1)}>Falsy Value</button>
+      <button className="button" onClick={(e) => setInput(value1)}>Falsy Value</button>
       <br />
       <br />
-      <button onClick={(e) => setInput(value2)}>An Array</button>
+      <button className="button" onClick={(e) => setInput(value2)}>An Array</button>
       <br />
       <br />
-      <button onClick={(e) => setInput(value3)}>Anything Else</button>
+      <button className="button" onClick={(e) => setInput(value3)}>Anything Else</button>
       <br />
       <br />
       <ShowInput input={input}/>
-    </>
+    </div>
   );
 }
 
